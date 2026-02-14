5640b0cea5b94e028daabb387f29c35777d66a2c
Poulomi0789 <50943118+Poulomi0789@users.noreply.github.com>
2026-02-14 21:37:58 +0530

    Update LoginPage.java

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -9,18 +9,18 @@ import org.openqa.selenium.support.FindBy;
 public class LoginPage extends BasePage {
 
     @FindBy(id = "user-name") // WEB
-    @AndroidFindBy(accessibility = "test-Username") // ANDROID
-    @iOSXCUITFindBy(accessibility = "test-Username") // IOS
+    // @AndroidFindBy(accessibility = "test-Username") // ANDROID
+    // @iOSXCUITFindBy(accessibility = "test-Username") // IOS
     private WebElement usernameField;
 
     @FindBy(id = "password")
-    @AndroidFindBy(accessibility = "test-Password")
-    @iOSXCUITFindBy(accessibility = "test-Password")
+    // @AndroidFindBy(accessibility = "test-Password")
+    // @iOSXCUITFindBy(accessibility = "test-Password")
     private WebElement passwordField;
 
     @FindBy(id = "login-button")
-    @AndroidFindBy(accessibility = "test-LOGIN")
-    @iOSXCUITFindBy(accessibility = "test-LOGIN")
+    // @AndroidFindBy(accessibility = "test-LOGIN")
+    // @iOSXCUITFindBy(accessibility = "test-LOGIN")
     private WebElement loginButton;
 
     public LoginPage(WebDriver driver) { super(driver); }
